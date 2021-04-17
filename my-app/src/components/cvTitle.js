a5586040f6e5cde6bd80c2ea9ccf9bad0b609c53
Gavin <yzh_xjy@126.com>
2021-04-17 20:58:48 +0800

    React version

---
+++ my-app/src/components/cvTitle.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.css';
+
+const cvTitle = () => {
+    return (
+        <div className = "title">
+            <div className = "myName">
+                Gavin Yang
+            </div>
+            <div className = "contact">
+                phone: 6175151831 email: zy2091@nyu.edu
+            </div>
+        </div>
+    );
+
+}
+
+export default cvTitle;
